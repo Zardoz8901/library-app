bfe304485753e59a708d46286a5f7fe0b864669e
Zardoz8901 <interzoneclearinghouse@gmail.com>
2023-03-06 16:03:08 +0100

    Apply pain to relearn JS functions and array methods

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const Library = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-
-console.log(theHobbit);
-
-function addBookToLibrary() {}
-
 const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".button-open");
 const closeModal = document.querySelector(".button-close");
@@ -25,6 +10,49 @@ closeModal.addEventListener("click", () => {
   modal.close();
 });
 
-const book = document.querySelector(".book > div > span");
+const bookAuthor = document.querySelector(".book-author > span");
 
-console.log(book);
+const bookName = document.querySelector(".book-title > span");
+
+const bookPages = document.querySelector(".book-pages > span");
+
+const bookNode = document.querySelector("#book");
+
+const bookClone = bookNode.cloneNode(true);
+
+console.log(bookClone);
+
+const inputAuthor = document.getElementById("author");
+
+const inputTitle = document.getElementById("title");
+
+const inputPages = document.getElementById("pages");
+
+const inputRead = document.getElementById("read");
+
+const inputUnread = document.getElementById("unread");
+
+const submitButton = document.getElementById("submit");
+
+const Library = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function inputToValue() {
+  const author = inputAuthor.value;
+  const title = inputTitle.value;
+  const pages = Number(inputPages.value);
+  const read = inputRead.checked;
+  const book = new Book(author, title, pages, read);
+  return Library.unshift(book);
+}
+
+submitButton.addEventListener("click", (e) => {
+  inputToValue();
+  console.log(Library);
+});
